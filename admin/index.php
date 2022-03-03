921b6febe95ecff65a8ba1bacc30044cc80e1b00
Jeffrey GEROMEGNACE <jeffrey.geromegnace@gmail.com>
2022-03-03 16:27:29 +0100

    MODIFS

--- admin/index.php
+++ admin/index.php
@@ -6,12 +6,6 @@ require('../inc/request.php');
 include('../inc/header.php'); 
 $errors = array();
 
-<<<<<<< HEAD
 include('inc/header.php'); ?>
-=======
-?>
-    <div class="wrap">
-        <h2>Admin Home Page</h2>
->>>>>>> 27a7e4e7a5fbe122719a550a24a9c612e25818b2
 
 <?php include('inc/footer.php');
