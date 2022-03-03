1156f5581cea00fe2cd3aef9b1fcddd10e1145ae
DavidB Zodar <zodarfr@gmail.com>
2022-03-03 12:56:48 +0100

    recup

--- view/viewcomment.php
+++ view/viewcomment.php
@@ -1,23 +1,23 @@
 
 <?php  
 if(!empty($_GET['id']) && is_numeric($_GET['id'])) {
-            $id = $_GET['id'];
+    $id = $_GET['id'];
 }
-        $sql = "SELECT * FROM blog_comments";
-        $query = $pdo->prepare($sql);
-        
-        $query->execute();
-        $comments = $query->fetchAll();
-        debug($comments);
-    ?>
-        <section id="comments">
-            <?php foreach ($comments as $comment) { ?>
-                <div class="one_article" id="ancre-<?= $comment['id']; ?>">
-                    <div>
-                        <hr>
-                        <h2><?php echo $comment['id']; ?></h2>
-                        <hr>
-                    </div>
-                </div>
-            <?php } ?>
-        </section
+$sql = "SELECT * FROM blog_comments";
+$query = $pdo->prepare($sql);
+
+$query->execute();
+$articles = $query->fetchAll();
+// debug($articles);
+?>
+<section id="articles">
+    <?php foreach ($articles as $article) { ?>
+        <div class="one_article" id="ancre-<?= $article['id']; ?>">
+            <div>
+                <hr>
+                <h2><?php echo $article['content']; ?></h2>
+                <hr>
+            </div>
+        </div>
+    <?php } ?>
+</section>
