d6fa7337760d60204e6d7997769f87f9b226d1e6
DavidB Zodar <zodarfr@gmail.com>
2022-03-03 16:13:05 +0100

    ajout des action dans admin: ajouter un article et retourner sur lindex principal

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,5 +1,4 @@
 
-
 <?php
 require('../inc/pdo.php');
 // require('inc/fonction.php');
@@ -78,10 +77,10 @@ foreach ($users as $user) {
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                <a class="nav-link collapsed" href="./editpost.php" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Creer un article</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
@@ -94,10 +93,10 @@ foreach ($users as $user) {
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                <a class="nav-link collapsed" href="../index.php" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <span>Retour espace Acceuil</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
--- admin/index.php
+++ admin/index.php
@@ -3,15 +3,6 @@ session_start();
 require('../inc/pdo.php');
 require('../inc/fonction.php');
 require('../inc/request.php');
-include('../inc/header.php'); 
+include('inc/header.php'); 
 $errors = array();
-
-<<<<<<< HEAD
-include('inc/header.php'); ?>
-=======
-?>
-    <div class="wrap">
-        <h2>Admin Home Page</h2>
->>>>>>> 27a7e4e7a5fbe122719a550a24a9c612e25818b2
-
-<?php include('inc/footer.php');
+include('inc/footer.php');
